271fe9b686486b64e7f57d1b08fe06363809dd7c
xiaowenzhan <576388451@qq.com>
2018-11-15 17:35:16 +0800

    首次提交到本地仓库

---
+++ gititem/src/main/java/com/zzh/GititemApplication.java
@@ -0,0 +1,12 @@
+package com.zzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GititemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GititemApplication.class, args);
+	}
+}
